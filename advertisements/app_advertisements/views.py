c027320cdd685ed6e4ae06fb7a844001b4b0311b
Rustic <r.fahrislamov08@gmail.com>
2023-08-23 20:21:17 +0500

    lesson9

---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Advertisements
+
+def index(request):
+    advertisements = Advertisements.objects.all()
+    context = {"advertisements":advertisements}
+    return render(request,'index.html',context)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
